30e49d5b2ab0447ad07c222a59d9e15c9ff4ee05
dhruv_v <jagatjyoti0810@gmail.com>
2025-01-15 11:58:42 +0530

    added gen count

--- flappy_bird.py
+++ flappy_bird.py
@@ -8,6 +8,8 @@ pygame.font.init()
 WIN_WIDTH=550
 WIN_HEIGHT=800
 
+GEN=0
+
 BIRD_IMGS=[pygame.transform.scale2x(pygame.image.load(os.path.join("imgs","bird1.png"))),pygame.transform.scale2x(pygame.image.load(os.path.join("imgs","bird2.png"))),pygame.transform.scale2x(pygame.image.load(os.path.join("imgs","bird3.png")))]
 PIPE_IMGS=pygame.transform.scale2x(pygame.image.load(os.path.join("imgs","pipe.png")))
 BASE_IMGS=pygame.transform.scale2x(pygame.image.load(os.path.join("imgs","base.png")))
@@ -147,7 +149,7 @@ class Base:
         win.blit(self.IMG,(self.x2,self.y))
 
     
-def draw_window(win,birds,pipes,base,score):
+def draw_window(win,birds,pipes,base,score,gen):
     win.blit(BG_IMGS,(0,0))
 
     for pipe in pipes:
@@ -156,12 +158,17 @@ def draw_window(win,birds,pipes,base,score):
     text=STAT_FONT.render("Score: "+str(score),1,(255,255,255))
     win.blit(text,(WIN_WIDTH-10-text.get_width(),10))
 
+    text=STAT_FONT.render("Gen: "+str(gen),1,(255,255,255))
+    win.blit(text,(10,10))
+
     base.draw(win)
     for bird in birds:
         bird.draw(win)
     pygame.display.update()
 
 def main(genomes,config):
+    global GEN
+    GEN+=1
     nets=[]
     ge=[]
     birds=[]
@@ -253,7 +260,7 @@ def main(genomes,config):
                 nets.pop(x)
                 ge.pop(x)
         
-        draw_window(win,birds,pipes,base,score)
+        draw_window(win,birds,pipes,base,score,GEN)
     
 
 
